fda06b18fab7cae421787597b668e21789abaa51
Isaac Carcare <70181194+Isaacc1018@users.noreply.github.com>
2022-01-29 14:01:18 -0500

    character sprite mirroring on movement and punching animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -13,13 +13,25 @@ func get_input():
 	velocity.x = 0
 	if Input.is_action_pressed("Right"):
 		velocity.x += moveSpeed
+		$AnimatedSprite.scale.x = 1
 	if Input.is_action_pressed("Left"):
 		velocity.x -= moveSpeed
+		$AnimatedSprite.scale.x = -1
 	if Input.is_action_just_pressed("Flip"):
 		gravity = gravity * -1
 		jumpStr = jumpStr * -1
 		position.y=position.y*-1
-		$PlaceHolderSprite.scale.y = $PlaceHolderSprite.scale.y*-1
+		$AnimatedSprite.scale.y = $AnimatedSprite.scale.y*-1
+	if Input.is_action_just_pressed("Punch"):
+		$AnimatedSprite.frame=1
+		var t = Timer.new()
+		t.set_wait_time(0.5)
+		t.set_one_shot(true)
+		self.add_child(t)
+		t.start()
+		yield(t, "timeout")
+		t.queue_free()
+		$AnimatedSprite.frame=0
 
 func _physics_process(delta):
 	get_input()
--- Player/Shadow.gd
+++ Player/Shadow.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends AnimatedSprite
 
 var player = null
 var binded= false
