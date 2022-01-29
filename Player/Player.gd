714b70b188311a92c9c24f26a317d13b91beec76
Isaac Carcare <70181194+Isaacc1018@users.noreply.github.com>
2022-01-29 13:47:50 -0500

    added player controls and mirroring

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const gravity = 200
+var gravity = 200
 const moveSpeed = 250
-const jumpStr = -250
+var jumpStr = -250
 var velocity = Vector2.ZERO
 
 
@@ -15,13 +15,18 @@ func get_input():
 		velocity.x += moveSpeed
 	if Input.is_action_pressed("Left"):
 		velocity.x -= moveSpeed
+	if Input.is_action_just_pressed("Flip"):
+		gravity = gravity * -1
+		jumpStr = jumpStr * -1
+		position.y=position.y*-1
+		$PlaceHolderSprite.scale.y = $PlaceHolderSprite.scale.y*-1
 
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity,Vector2.UP)
 	if Input.is_action_just_pressed("Jump"):
-		if is_on_floor():
+		if is_on_floor() || is_on_ceiling():
 			velocity.y = jumpStr
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
---
+++ Player/Shadow.gd
@@ -0,0 +1,27 @@
+extends Sprite
+
+var player = null
+var binded= false
+
+
+func _ready():
+	pass
+
+
+
+func _physics_process(_elta):
+	if Input.is_action_just_pressed("Flip"):
+		scale.y = scale.y * -1
+	if player != null:
+		position.x = player.position.x
+		position.y = player.position.y * -1
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if binded == false || body == KinematicBody2D:
+		player = body
+		binded = true
