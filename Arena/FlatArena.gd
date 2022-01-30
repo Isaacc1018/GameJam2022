b8f777adb20010fce575824e9679aacf70a0744b
eforrest8 <ethan.forrest@bsu.edu>
2022-01-30 15:31:33 -0500

    Add FlatArena.gd

---
+++ Arena/FlatArena.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_P1_health_zero():
+	pass # Replace with function body.
+
+
+func _on_P2_health_zero():
+	pass # Replace with function body.
